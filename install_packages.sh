f2907d8bc670d77ec2062df32a4407b2b681ccaa
XenonTheInertG <XenonTheInertG@outlook.com>
2022-06-18 16:42:36 +0600

    Docky: add libneeds & idcheck

--- install_packages.sh
+++ install_packages.sh
@@ -4,35 +4,50 @@
 sed -i "/\[multilib\]/,/Include/"'s/^#//' /etc/pacman.conf
 
 # Update
-pacman -Syyu --needed --noconfirm
+pacman -Syyu --needed --noconfirm 2>&1 | grep -v "warning: could not get file information"
 
 # Install Basic Packages
-pacman -S --needed --noconfirm \
+pacman -Sy --needed --noconfirm \
 	sudo nano git curl wget rsync aria2 rclone \
 	python2 python3 python-pip zip unzip cmake \
 	make neofetch speedtest-cli inetutils cpio \
 	jdk8-openjdk lzip dpkg openssl ccache repo \
 	libelf base-devel openssh lz4 jq go ncurses \
 	bison flex ninja uboot-tools z3 glibc dpkg \
-	multilib-devel bc htop
+	multilib-devel bc htop python-setuptools   \
+	util-linux man man-pages zsh
 
 # More Packages
-pacman -S --needed --noconfirm \
+pacman -Sy --needed --noconfirm \
 	tmate tmux screen mlocate unace unrar p7zip \
 	sharutils uudeview arj cabextract file-roller \
-	dtc brotli axel gawk detox clang gcc gcc-libs
+	dtc brotli axel gawk detox clang gcc gcc-libs \
+	flatpak
 
-# pip version
-pip --version
+# python and pip version
+python --version; pip --version
 
 # Install Some pip packages
 pip install \
 	twrpdtgen telegram-send backports.lzma docopt \
-	extract-dtb protobuf pycrypto docopt zstandard
+	extract-dtb protobuf pycrypto docopt zstandard \
+	setuptools
+
+# pip git packages
+pip install \
+	git+https://github.com/samloader/samloader.git
 
 # Create a non-root user for AUR
 useradd -m -G wheel -s /bin/bash testuser
 echo "%wheel ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers
+
+# AUR Packages
+sudo -u testuser yay -S --needed --noconfirm \
+	rename
+
+# zsh
+chsh -s /bin/zsh root
+curl -sL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh | bash
 
 # Setup the Android Build Environment
 cd /tmp/scripts
@@ -40,5 +55,10 @@ sudo chmod -R a+rwx .
 sudo -u testuser bash ./aosp-build-env.sh
 cd -
 
+# Python Symlinks
+ln -sf /usr/bin/pip3.10 /usr/bin/pip3
+ln -sf /usr/bin/pip3.10 /usr/bin/pip
+ln -sf /usr/bin/python3.10 /usr/bin/python3
+
 # Use python2 by default
-ln -sf /usr/bin/python2 /usr/bin/python
+ln -sf /usr/bin/python{2,}
